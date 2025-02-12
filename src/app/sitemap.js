c1d2a3bb75b1a9dd0c1c94b8e8c7fcc82782ebb1
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-02-12 15:15:04 +0530

    sitemap update

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,25 +1,31 @@
 export default function sitemap() {
-    const baseUrl = 'https://toolscloud.pankri.com';
-  
-    const pages = [
-      '/word-character-counter/tool',
-      '/find-and-replace/tool',
-      '/text-case-converter/tool',
-      '/text-to-speech/tool',
-      '/text-editor/tool',
-      '/text-beautifier/tool',
-      '/spell-checker/tool',
-      '/grammar-checker/tool',
-      '/quote-validator/tool',
-      '/text-duplicator-checker/tool',
-      
-    ];
-  
-    return pages.map((page) => ({
-      url: `${baseUrl}${page}`,
-      lastModified: new Date().toISOString(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    }));
-  }
-  
+  const baseUrl = "https://toolscloud.pankri.com";
+
+  const pages = [
+    "/word-character-counter/tool",
+    "/find-and-replace/tool",
+    "/text-case-converter/tool",
+    "/text-to-speech/tool",
+    "/text-editor/tool",
+    "/text-beautifier/tool",
+    "/spell-checker/tool",
+    "/grammar-checker/tool",
+    "/quote-validator/tool",
+    "/text-duplicator-checker/tool",
+
+    "/hash-generator/tool",
+    "/uuid-generator/tool",
+    "/barcode-generator/tool",
+    "/qr-code-generator/tool",
+    "/lorem-ipsum-generator/tool",
+    "/random-number-generator/tool",
+    "/random-password-generator/tool",
+  ];
+
+  return pages.map((page) => ({
+    url: `${baseUrl}${page}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "monthly",
+    priority: 0.7,
+  }));
+}
